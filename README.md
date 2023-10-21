1b522ea8896f6b79efbd445a7115b00280fcb455
OmarRhodium <131199354+OmarRhodium@users.noreply.github.com>
2023-10-21 16:25:24 -0500

    agregre hola mundo en node

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rosauraEjemplo
+hola mundo
---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('¡Hola, mundo!\n');
+});
+
+const port = 3001;
+server.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port}`);
+});
